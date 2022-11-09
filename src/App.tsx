db381ffa8eab8fe3c1c9bf7231957dc205705f9a
Santiago Weihmüller <61631736+SantiW01@users.noreply.github.com>
2022-11-09 17:17:29 -0300

    Adding Calculator's style

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Button, ButtonGroup } from 'react-bootstrap'
+import './Styles/App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function Calculadora() {
+  const [firstNumber, setFirstNumber] = useState<number>(0);
+  const [secondNumber, setSecondNumber] = useState<number>(0);
+  const result: number = 0;
+  return(
+    <div>
+      <div className='card rounded-0' id="body">
+        <div>
+          <p className='text-end text-bg-info'>{result}</p>
+        </div>
+        <div>
+          <ButtonGroup className='mb-2'>
+            <Button className='rounded-0 m-2' >1</Button>
+            <Button className='rounded-0 m-2' >2</Button>
+            <Button className='rounded-0 m-2' >3</Button>
+            <Button className='rounded-0 m-2' >+</Button>
+          </ButtonGroup>
+          <br />
+          <ButtonGroup className='mb-2'>
+            <Button className='rounded-0 m-2' >4</Button>
+            <Button className='rounded-0 m-2' >5</Button>
+            <Button className='rounded-0 m-2' >6</Button>
+            <Button className='rounded-0 m-2' >-</Button>
+          </ButtonGroup>
+          <br />
+          <ButtonGroup className='mb-2'>
+            <Button className='rounded-0 m-2' >7</Button>
+            <Button className='rounded-0 m-2' >8</Button>
+            <Button className='rounded-0 m-2' >9</Button>
+            <Button className='rounded-0 m-2' >*</Button>
+          </ButtonGroup>
+          <br />
+          <ButtonGroup className='mb-2'>
+          <Button className='rounded-0 m-2'>0</Button>
+            <Button className='rounded-0 m-2' >,</Button>
+            <Button className='rounded-0 m-2' >=</Button>
+            <Button className='rounded-0 m-2' >/</Button>
+          </ButtonGroup>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
