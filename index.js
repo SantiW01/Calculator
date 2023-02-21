7e027a12cd5752903d7aef6c90a7c5671e0ac789
SantiW01 <sweihmuller01@gmail.com>
2023-02-21 20:03:33 -0300

    feat: create operate's function[C

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ const multiply = function multiply(firstNumber, secondNumber) {
 const divide = function divide(firstNumber, secondNumber) {
   return firstNumber / secondNumber;
 };
+
+function MakeOperation(operate, firstNumber, secondNumber) {
+  switch (operate) {
+    case "+":
+      add(firstNumber, secondNumber);
+      break;
+    case "-":
+      subtract(firstNumber, secondNumber);
+      break;
+    case "*":
+      multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      divide(firstNumber, secondNumber);
+      break;
+  }
+}
