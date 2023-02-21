67290f7152fe5b82341fd789b7818bea4c13954e
SantiW01 <sweihmuller01@gmail.com>
2023-02-21 20:01:18 -0300

    feat: create function for each operator's buttons

---
+++ index.js
@@ -0,0 +1,12 @@
+const add = function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+};
+const subtract = function subtract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+};
+const multiply = function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+};
+const divide = function divide(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+};
