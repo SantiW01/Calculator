14bc3354d9525d72b75d4303e05e9a6a58183480
SantiW01 <sweihmuller01@gmail.com>
2023-02-22 13:45:24 -0300

    feat: Display number chosen on calculator's screen

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const divide = function divide(firstNumber, secondNumber) {
   return firstNumber / secondNumber;
 };
 
+let numberArray = [];
+let count = 0;
+let buttonPressed = false;
+
 function MakeOperation(operate, firstNumber, secondNumber) {
   switch (operate) {
     case "+":
@@ -27,3 +31,16 @@ function MakeOperation(operate, firstNumber, secondNumber) {
       break;
   }
 }
+
+function DisplayNumber(number) {
+  if (count < 8) {
+    numberArray.push(number);
+    count += 1;
+  }
+  document.querySelector(".calculator_screen_number").innerHTML =
+    numberArray.join("");
+
+  return numberArray.join("");
+}
+
+DisplayNumber();
