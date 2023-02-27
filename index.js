825fdfebeab920ec6951e6850fe4f1873a3be03e
SantiW01 <sweihmuller01@gmail.com>
2023-02-27 19:10:21 -0300

    feat: Make operations and display result

--- index.js
+++ index.js
@@ -1,46 +1,83 @@
 const add = function add(firstNumber, secondNumber) {
-  return firstNumber + secondNumber;
+  result = Number(firstNumber) + Number(secondNumber);
+  document.querySelector(".calculator_screen_number").innerHTML = result;
+  return result;
 };
 const subtract = function subtract(firstNumber, secondNumber) {
-  return firstNumber - secondNumber;
+  result = Number(firstNumber) - Number(secondNumber);
+  document.querySelector(".calculator_screen_number").innerHTML = result;
+  return result;
 };
 const multiply = function multiply(firstNumber, secondNumber) {
-  return firstNumber * secondNumber;
+  result = Number(firstNumber) * Number(secondNumber);
+  document.querySelector(".calculator_screen_number").innerHTML = result;
+  return result;
 };
 const divide = function divide(firstNumber, secondNumber) {
-  return firstNumber / secondNumber;
+  result = Number(firstNumber) / Number(secondNumber);
+  document.querySelector(".calculator_screen_number").innerHTML = result;
+  return result;
 };
 
 let numberArray = [];
 let count = 0;
 let buttonPressed = false;
+let firstNumber = 0;
+let secondNumber = 0;
+let result = 0;
+let operate = "";
 
-function MakeOperation(operate, firstNumber, secondNumber) {
-  switch (operate) {
-    case "+":
-      add(firstNumber, secondNumber);
-      break;
-    case "-":
-      subtract(firstNumber, secondNumber);
-      break;
-    case "*":
-      multiply(firstNumber, secondNumber);
-      break;
-    case "/":
-      divide(firstNumber, secondNumber);
-      break;
-  }
-}
+document.querySelectorAll(".calcualtor_number_button").forEach((e) => {
+  e.addEventListener("click", () => {
+    if (buttonPressed == false) {
+      if (count < 8) {
+        numberArray.push(e.innerHTML);
+        firstNumber = numberArray.join("");
+        document.querySelector(".calculator_screen_number").innerHTML =
+          firstNumber;
+        count++;
+      }
+    } else {
+      count = 0;
+      numberArray = [];
+      if (count < 8) {
+        numberArray.push(e.innerHTML);
+        secondNumber = numberArray.join("");
+        document.querySelector(".calculator_screen_number").innerHTML =
+          secondNumber;
+        count++;
+      }
+    }
+  });
+});
 
-function DisplayNumber(number) {
-  if (count < 8) {
-    numberArray.push(number);
-    count += 1;
-  }
-  document.querySelector(".calculator_screen_number").innerHTML =
-    numberArray.join("");
+document.querySelectorAll(".calcualtor_operator_button").forEach((e) => {
+  e.addEventListener("click", function () {
+    buttonPressed = true;
+    operate = e.innerHTML;
+  });
+});
 
-  return numberArray.join("");
-}
-
-DisplayNumber();
+document
+  .querySelector(".calcualtor_calculate_button")
+  .addEventListener("click", function () {
+    if (firstNumber == null || secondNumber == null || operate == null) {
+      return "false";
+    }
+    switch (operate) {
+      case "+":
+        firstNumber = add(firstNumber, secondNumber);
+        break;
+      case "-":
+        firstNumber = subtract(firstNumber, secondNumber);
+        break;
+      case "*":
+        firstNumber = multiply(firstNumber, secondNumber);
+        break;
+      case "/":
+        firstNumber = divide(firstNumber, secondNumber);
+        break;
+      default:
+        break;
+    }
+  });
